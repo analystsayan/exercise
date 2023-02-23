d0da93d6d466dcd4ccb56035347b9553b263a722
captainsayan <captainsayan7550@gmail.com>
2023-02-23 13:02:02 +0530

    file name change

--- code_basics/2_variables/soluation1.py
+++ code_basics/2_variables/soluation1.py
@@ -1,7 +1,7 @@
 # break = 5
 
 # it gives :
-#            File "D:\Works\pycharm\exercise\2_variables\soluation.py", line 1
+#            File "D:\Works\pycharm\exercise\2_variables\soluation1.py", line 1
 #     break = 5
 #           ^
 # SyntaxError: invalid syntax
